820706918204deed694ba4898fec184ad2f37567
Ubuntu <ubuntu@ip-172-31-2-82.ec2.internal>
2022-06-16 14:08:42 +0000

    fixed a few issues after testing the module with the node reports microservice

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,7 +2,7 @@ export interface AdapterOptions {
   redisOptions: { url: string };
   inChannel: string;
   outChannel: string;
-  processMessage: (message: string | Buffer) => string | Buffer;
+  processMessage: (message: string | Buffer) => string | Buffer | Promise<string | Buffer>;
   handleError: (error: Error) => void;
   isBufferMode?: boolean;
 }
--- tests/integrationTests.ts
+++ tests/integrationTests.ts
@@ -1,5 +1,6 @@
+import * as assert from 'assert';
 import * as path from 'path';
-import { Worker, isMainThread,  } from 'worker_threads';
+import { Worker, isMainThread, parentPort } from 'worker_threads';
 import { createClient } from 'redis';
 import { setUpAdapter } from '../src/index';
 import { AdapterOptions } from '../src/interfaces';
@@ -17,22 +18,32 @@ const options: AdapterOptions = {
 if (isMainThread) {
   const worker = new Worker(__filename);
   setUpAdapter(options);
+  worker.on('message', () => { process.exit(0);});
 } else {
   const listener = createClient(options.redisOptions);
   listener.connect()
     .then(function () {
+      let i = 2;
       listener.subscribe(options.outChannel, function(message: string | Buffer) {
-        console.log('LISTENER: ' + message.toString('utf-8'));
+        const data = JSON.parse(message.toString('utf-8'));
+        assert.equal(data.count, i++);
+        if (11 === i) {
+          console.log('ALL TESTS HAVE PASSED.');
+          // @ts-ignore
+          parentPort.postMessage('done');
+          listener.quit();
+        }
       }, options.isBufferMode || true);
     })
     .then(function () {
       const publisher = listener.duplicate();
       publisher.connect().then(function () {
-        const data = JSON.stringify({
-          count: 0
+        [...range(1, 10)].forEach(function (x) {
+          const data = JSON.stringify({
+            count: x
+          });
+          publisher.publish(options.inChannel, data);
         });
-        console.log('PUBLISHER: ' + data);
-        publisher.publish(options.inChannel, data);
       });
     })
     .catch(function (err) { console.error(err); });
@@ -40,9 +51,11 @@ if (isMainThread) {
 
 function processMessage(message: string | Buffer): string | Buffer {
   let data = JSON.parse(message.toString('utf-8'));
-  data.count += 42;
+  data.count += 1;
   data = JSON.stringify(data);
-  console.log('The message has been processed.');
   return Buffer.from(data);
 }
 
+function *range(start: number, end: number): any {
+  for (let i = start; i < end; ++i) { yield i; }
+}
